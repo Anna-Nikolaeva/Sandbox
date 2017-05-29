8f42e09ba83ff32207d6d0932d9f87c05c708c64
DESKTOP-84N9KUD\anna8 <anna88nikolaeva@gmail.com>
2017-05-29 20:18:25 +0300

    First test

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by anna8 on 29.05.2017.
+ */
+public class Main {
+
+
+    public static void main(String args[ ])
+    {
+        System.out.println( "Welcome to the world of Java" );
+    }
+}
